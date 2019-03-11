e755efa4390d819ec4019147bd587fe3e44e364d
venkatesh hegde <venkateshhegde@yahoo.com>
2019-03-11 15:34:41 -0400

    Addd hello world

--- SpringStuff/src/main/java/com/example/demo/SpringStuffApplication.java
+++ SpringStuff/src/main/java/com/example/demo/SpringStuffApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringStuffApplication {
+	
+	public SpringStuffApplication() {
+	System.out.println("HELLO SPRING");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStuffApplication.class, args);
