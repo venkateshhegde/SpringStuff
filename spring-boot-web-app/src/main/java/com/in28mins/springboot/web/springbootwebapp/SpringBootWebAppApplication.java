a81a117e1b043af73ba97842c4621a3d3d87e9c2
venkatesh hegde <venkateshhegde@yahoo.com>
2019-03-12 13:17:49 -0400

    added

---
+++ spring-boot-web-app/src/main/java/com/in28mins/springboot/web/springbootwebapp/SpringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.in28mins.springboot.web.springbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppApplication.class, args);
+	}
+
+}
