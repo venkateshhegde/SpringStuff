885ac16f9f5a4eebfed4a68056a3843283035b2b
venkatesh hegde <venkateshhegde@yahoo.com>
2019-03-12 13:17:11 -0400

    Added Bus logic

--- spring-boot-web-app/src/main/java/com/in28mins/springboot/web/springbootwebapp/controller/LoginController.java
+++ spring-boot-web-app/src/main/java/com/in28mins/springboot/web/springbootwebapp/controller/LoginController.java
@@ -1,19 +1,47 @@
 package com.in28mins.springboot.web.springbootwebapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.in28mins.springboot.web.springbootwebapp.service.LoginService;
+
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
-	public String  loginMessage(@RequestParam  String name1, ModelMap model )
+	
+	@Autowired 
+	LoginService loginService;
+	
+	
+	@RequestMapping(value="/login", method= RequestMethod.POST)
+	public String  loginMessage1( ModelMap model, @RequestParam String name, @RequestParam String password )
 	{
-		System.out.println(name1);
-		model.put("NAME", name1);
+//		System.out.println(name);
+//		model.put("NAME", name);
+		model.put("NAME", name);
+		model.put("PASSWORD", password);
+		if (loginService.validate(name, password))
+		{
+			System.out.println("GOOD !!!! " + name + " "+   password);
+			model.put("good", "GOOD!");
+			return "login";	
+		}
+
+
+		System.out.println("FAILED!!!! " + name + " "+   password);
+		model.put("error", "bad user / pass");
 		return "login";
 	}
 
+	@RequestMapping(value="/login", method= RequestMethod.GET)
+	public String  loginMessage( ModelMap model)
+	{
+//		System.out.println(name);
+		return "welcome";
+	}
 }
--- spring-boot-web-app/src/main/webapp/WEB-INF/jsp/login.jsp
+++ spring-boot-web-app/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,11 +1,19 @@
 <html>
 <head>
-<title>HEADER Welcome ${NAME}</title>
+<title>HEADER</title>
 </head>
 <body>
+<p>
+<font color=red> ${error}</font>
+<p>
+<font color=green> ${good}</font>
 
-First JSP! Welcome ${NAME}
+<form method=post >
+<br>Name: <input name="name" type=text>
+<br>Password:<input name="password" type=password>
+<input type=submit>
+/<form>
 </body>
 
-<%out.println("H<i>ELLO</i> WORLD"); %> 
+ 
 </html>
